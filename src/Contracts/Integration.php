36f809eb1c86f4048863a207e4c95d937321d264
Oliver Wycisk <oliver.wycisk@gmail.com>
2020-10-07 11:23:52 +0200

    Added phpdoc.

--- src/Contracts/Integration.php
+++ src/Contracts/Integration.php
@@ -8,11 +8,17 @@ interface Integration
 {
 
 	/**
-	 * Returns the identifier name for this integration
+	 * Returns the identifier name for this integration.
+	 *
 	 * @returns string
 	 */
 	public function getKey(): string;
 
+	/**
+	 * Returns the default config for an integration.
+	 *
+	 * @return array
+	 */
 	public function getDefaultSettings(): array;
 
 }
