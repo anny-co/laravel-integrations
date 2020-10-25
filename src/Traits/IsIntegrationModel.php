8d89380dcb3bc7fd7d08cbe4b42e09fc5aaf717d
Oliver Wycisk <oliver.wycisk@gmail.com>
2020-10-25 16:54:22 +0100

    Added trait for integration model

---
+++ src/Traits/IsIntegrationModel.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace Bddy\Integrations\Traits;
+
+
+trait IsIntegrationModel
+{
+	/**
+	 * @var string
+	 */
+	protected $integrationKeyKey = 'key';
+
+	/**
+	 * Casts
+	 *
+	 * @var string[]
+	 */
+	protected $casts = [
+		'active'                  => 'boolean',
+		'settings'                => 'json',
+		'authentication_required' => 'boolean',
+	];
+
+	protected function getIntegrationKey()
+	{
+		return $this->getAttribute($this->integrationKeyKey);
+	}
+
+	/**
+	 * Relation to integratable model
+	 * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+	 */
+	public function model()
+	{
+		return $this->morphTo('model');
+	}
+
+	/**
+	 * Activate a specific integration model.
+	 *
+	 * @return mixed
+	 */
+	public function activateIntegration()
+	{
+		integrations()
+			->getIntegration($this->getIntegrationKey())
+			->activate($this);
+	}
+
+	/**
+	 * Deactivate a specific integration model.
+	 *
+	 * @return mixed
+	 */
+	public function deactivateIntegration()
+	{
+		integrations()
+			->getIntegration($this->getIntegrationKey())
+			->activate($this);
+	}
+
+	/**
+	 * Initialize a specific integration model.
+	 *
+	 * @return mixed
+	 */
+	public function initializeIntegration()
+	{
+		integrations()
+			->getIntegration($this->getIntegrationKey())
+			->activate($this);
+	}
+}
