5192956dcfda4df252009268a51f18849d7d3aae
Oliver Wycisk <oliver.wycisk@gmail.com>
2022-02-22 18:05:00 +0100

    fix: missing methods

--- src/Models/Integration.php
+++ src/Models/Integration.php
@@ -6,6 +6,7 @@ use Anny\Integrations\Contracts\IntegrationModel;
 use Anny\Integrations\Traits\EncryptsSettings;
 use Anny\Integrations\Traits\IsIntegrationModel;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 class Integration extends Model implements IntegrationModel
 {
@@ -19,6 +20,29 @@ class Integration extends Model implements IntegrationModel
 
     public function setSecrets(array $secrets): static
     {
+        $this->secrets = $secrets;
+
+        return $this;
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return array|\ArrayAccess|mixed
+     */
+    public function getSecret(string $key, $default = null): mixed
+    {
+        return Arr::get($this->secrets, $key, $default);
+    }
+
+    /**
+     * @param string $key
+     * @param        $value
+     */
+    public function setSecret(string $key, $value): static
+    {
+        $secrets = $this->secrets;
+        Arr::set($secrets, $key, $value);
         $this->secrets = $secrets;
 
         return $this;
